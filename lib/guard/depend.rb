3c597abbe4976b77560369f9dc802ff4c1013ca2
Alexander Groß <agross@therightstuff.de>
2014-03-28 02:54:09 +0100

    Adding code

--- lib/guard/depend.rb
+++ lib/guard/depend.rb
@@ -1,7 +1,74 @@
-require "guard/depend/version"
+require 'guard'
+require 'guard/plugin'
 
 module Guard
-  module Depend
-    # Your code goes here...
+  class Depend < Plugin
+    require 'guard/depend/options'
+    require 'guard/depend/runner'
+
+    def initialize(options = {})
+      super
+      @options = Options.with_defaults(options)
+      @runner = Runner.new
+    end
+
+    def start
+      UI.info 'Guard::Depend is running'
+      run_all if @options[:run_on_start]
+    end
+
+    def stop
+    end
+
+    def reload
+    end
+
+    def run_all
+      @runner.run(@options[:cmd])
+    end
+
+    def run_on_changes(paths = [])
+      run_if_outdated(paths)
+    end
+
+    private
+    def run_if_outdated(paths = [])
+      return false if paths.empty?
+
+      outdated = out_of_date?(paths, @options[:output_paths])
+      unless outdated
+        UI.debug("Output is not outdated with regard to #{paths}")
+        return
+      end
+
+      @runner.run(@options[:cmd])
+    end
+
+    def out_of_date?(input, output)
+      output = output.call if output.respond_to?(:call)
+
+      return true if input.nil? || input.empty? || output.nil? || output.empty?
+
+      input = input.max_by {|f| input_mtime(f) }
+      output = output.min_by {|f| output_mtime(f) }
+
+      input_time = input_mtime(input)
+      output_time = output_mtime(output)
+
+      UI.debug("Newest input file:  #{input_time} #{input}")
+      UI.debug("Oldest output file: #{output_time} #{output}")
+
+      return input_time > output_time
+    end
+
+    def input_mtime(file)
+      return Time.new(9999, 12, 31) unless File.readable?(file)
+      File.mtime(file)
+    end
+
+    def output_mtime(file)
+      return Time.new(0, 1, 1) unless File.readable?(file)
+      File.mtime(file)
+    end
   end
 end
---
+++ lib/guard/depend/runner.rb
@@ -0,0 +1,29 @@
+require 'rake'
+
+module Guard
+  class Depend
+    class Runner
+      def run(cmd)
+        cmd = [cmd].flatten
+        command = cmd.join(' ')
+
+        begin
+          UI.info("Running '#{command}'")
+          Notifier.notify(command, title: 'Running', image: :success)
+
+          sh(*cmd)
+
+          Notifier.notify("#{command} succeeded", title: 'Success', image: :success)
+        rescue Exception => e
+          UI.error("Failed to run '#{command}'. Exception was: #{e.class}: #{e.message}")
+          UI.debug(e.backtrace.join("\n"))
+
+          Notifier.notify("#{command} failed with #{e}", title: 'Failed', image: :failed)
+        end
+      end
+
+      private
+      include FileUtils
+    end
+  end
+end
